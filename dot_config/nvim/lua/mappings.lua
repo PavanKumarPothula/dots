84ec9b12c73d70dede6d61d10e6e3a2cd6b55c51
Pavan Kumar Pothula <ppavanrockz@gmail.com>
2026-06-27 13:53:54 +0530

    added nvim and tmux

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -11,3 +11,8 @@ map("i", "jk", "<ESC>")
 -- map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
 local nomap = vim.keymap.del
 nomap("n", "<C-n>")
+
+map('n', '<leader>ls', '<cmd>Telescope lsp_document_symbols<cr>',
+  { desc = 'LSP: Document symbols' })
+map('n', '<leader>lS', '<cmd>Telescope lsp_dynamic_workspace_symbols<cr>',
+  { desc = 'LSP: Workspace symbols' })
